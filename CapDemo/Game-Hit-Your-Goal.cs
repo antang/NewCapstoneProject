7a24dd6289f43a975337fbdddf9c346be0b518d9
Quoc La Tao <dothanhwuoc@gmail.com>
2015-12-09 17:54:15 +0700

    update interface

---
+++ CapDemo/Game-Hit-Your-Goal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapDemo
+{
+    public partial class Game_Hit_Your_Goal : Form
+    {
+        public Game_Hit_Your_Goal()
+        {
+            InitializeComponent();
+        }
+    }
+}
